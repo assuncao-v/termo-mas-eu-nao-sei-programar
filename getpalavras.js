c7e381552837c2a35be22b13654b0c8007c73569
Alex Sandro <alexsandromacedopinto@gmail.com>
2024-05-06 17:23:05 -0300

    Funciona

--- getpalavras.js
+++ getpalavras.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
 
-const string = fs.readFileSync("palavras.txt", {encoding: "utf-8"}).split("\r\n").filter((value, i) => (value.length == 5 && value[0] != value[0].toUpperCase() && value.match(/[a-z]{5,5}/))).map((value) => ("\"" + value + "\"")).join(',\n');
+const string = fs.readFileSync("palavras.txt", {encoding: "utf-8"}).split("\r\n").filter((value, i) => (value.length == 5 && value[0] != value[0].toUpperCase() && value.match(/[a-z]{5,5}/) && value.match(/(a.*a.*a)|(e.*e.*e)|(i.*i.*i)|(o.*o.*o)|(u.*u.*u)/))).map((value) => ("\"" + value + "\"")).join(',\n');
 
 fs.writeFileSync("palavras.txt", string);
